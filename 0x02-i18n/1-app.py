e23bb45770f24c140d2a838cd905c5c6ffffbcae
bkaraba <bkaraba14@gmail.com>
2022-05-14 08:23:40 +0000

    task 2

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""babel config module"""
+
+
+import os
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config(object):
+    """configurations for babel instance"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object("1-app.Config")
+
+@app.route("/")
+def hello() -> str:
+    """root route"""
+    return render_template("1-index.html")
+
+if __name__ == "__main__":
+    host = os.getenv("API_HOST", "0.0.0.0")
+    port = os.getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
